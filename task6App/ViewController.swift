0e03ab4625b3003b11d1dcd1a93ebe81053a0ba9
yasu1125 <v22.m1125@gmail.com>
2021-04-04 23:38:32 +0900

    initial commit

--- task6App/ViewController.swift
+++ task6App/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    private var questionValue: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        random()
     }
 
+    private func random() {
 
+        for _ in 1...100 {
+            questionValue = Int.random(in: 1...100)
+        }
+
+        questionLabel.text = "\(questionValue)"
+
+        slider.value = 50
+    }
+
+    private func alert(title:String, message:String) {
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        alertController.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: { action in self.random() }))
+
+        present(alertController, animated: true)
+    }
+
+    @IBAction private func decisionButton(_ sender: Any) {
+
+        let number = Int(slider.value)
+
+        if questionValue == number {
+            alert(title: "結果", message: "当たり!\nあなたの値: \(number)")
+        }else{
+            alert(title: "結果", message: "はずれ!\nあなたの値: \(number)")
+        }
+    }
 }
 
